29e23a75c45a5e51c4be7a10c3a3f349abb00d5e
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2025-08-20 23:10:25 +0700

    UPDATE

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CreateWAController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,9 +11,14 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+|aa
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/{code}', [CreateWAController::class, 'link']);
+Route::get('/YourLink/{urlacak}', [CreateWAController::class, 'show']);
+Route::post('/create', [CreateWAController::class, 'store']);
+Route::post('/send', [CreateWAController::class, 'create']);
+Route::get('/', [CreateWAController::class, 'index']);
+Route::get('/URL-Spesial', [CreateWAController::class, 'generate']);
+
